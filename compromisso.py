e3325c33b6c96a06505a409236f1ae30a2e542ac
lunacae <lunacaemt@outlook.com>
2023-02-12 09:26:57 -0300

    Adicionado métodos para pesquisa

--- compromisso.py
+++ compromisso.py
@@ -30,10 +30,46 @@ class Compromisso:
     def getDescricao(self):
         return self.descricao
 
+def listarCompromisso(compromisso):
+    print('Data: ' + compromisso.data)
+    print('Hora: ' + compromisso.hora)
+    print('Duração: ' + compromisso.duracao)
+    print('Descrição: ' + compromisso.descricao)
+
+def listarCompromissos(vetor):
+    print('\nLista de compromissos: ')
+    for i in range(len(vetor)):
+        print('Id do compromisso: ', i)
+        print('Data: ' + vetor[i].data)
+        print('Hora: ' + vetor[i].hora)
+        print('Duração: ' + vetor[i].duracao)
+        print('Descrição: ' + vetor[i].descricao)
+
+def listarCompromissosPorData(vetor, data):
+    for i in range(len(vetor)):
+        if(vetor[i].data == data):
+            print('Id do compromisso: ', i)
+            listarCompromisso(vetor[i])
+
+def listarCompromissosPorDataeHora(vetor, data, hora):
+    for i in range(len(vetor)):
+        if(vetor[i].data == data and vetor[i].hora == hora):
+            print('Id do compromisso: ', i)
+            listarCompromisso(vetor[i])
+
+def adicionaCompromisso(vetor):
+    data = input('informe a data (dd/mm/aaaa): ')
+    hora = input('informe a hora (valor inteiro): ')
+    duracao = input('informe a duração em horas (valor inteiro): ')
+    descricao = input('informe a descrição: ')
+    comp = Compromisso(data, hora, duracao, descricao)
+    vetor.append(comp)
+    return vetor
+
 vetor = []
 c = 0
 while c != 7:
-    print('1 - Incluir compromisso')
+    print('\n1 - Incluir compromisso')
     print('2 - Consultar compromisso por data')
     print('3 - Consultar compromisso por hora e data')
     print('4 - Alterar compromisso')
@@ -43,28 +79,24 @@ while c != 7:
     c = int(input('informe qual operação deseja executar: '))
 
     if(c == 1):
-        data = input('informe a data (dd/mm/aaaa): ')
-        hora = input('informe a hora (valor inteiro): ')
-        duracao = input('informe a duração em horas (valor inteiro): ')
-        descricao = input('informe a descrição: ')
-        comp = Compromisso(data, hora, duracao, descricao)
-        vetor.append(comp)
+        vetor = adicionaCompromisso(vetor)
+
     elif(c == 2):
         data = input('Informe a data para pesquisa (dd/mm/aaaa): ')
+        listarCompromissosPorData(vetor, data)
     elif(c == 3):
         data = input('Informe a data para pesquisa (dd/mm/aaaa): ')
         hora = input('Informe a hora para pesquisa (valor inteiro): ')
+        listarCompromissosPorDataeHora(vetor, data, hora)
 
     elif(c == 4):
         print('Pensar em como buscar o compromisso e alterá-lo')
     elif(c == 5):
         print("Pensar em uma forma de deletar compromisso")
     elif(c == 6):
-        for i in range(len(vetor)):
-            print('Id do compromisso: ' + i)
-            print('Data: ' + vetor[i].data)
-            print('Hora: ' + vetor[i].hora)
-            print('Duração: ' + vetor[i].duracao)
-            print('Descrição: ' + vetor[i].descricao)
+        if(len(vetor) > 0):
+            listarCompromissos(vetor)
+        else:
+            print('Nenhum compromisso foi adicionado ainda \n')
 
 print('Programa finalizado')
